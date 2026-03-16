3ef7fa8fe3b9e1ff5f224b878163fa4a2d78953b
Vaibhav Udhane <vaibhavudhane1@gmail.com>
2026-03-16 17:58:02 +0530

    Day 1: Project setup, folder structure, DB connected, JWT dependency added

---
+++ src/test/java/com/pickleball/pickleball_backend/PickleballBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickleball.pickleball_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickleballBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
